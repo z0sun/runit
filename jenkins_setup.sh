503bf969b04e729c1bd0e2443e3e753bd4f4f228
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-05 20:01:06 -0400

    Update jenkins_setup.sh

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -7,7 +7,6 @@ sudo apt upgrade -y
 #enter the wait command to finish the update
 wait
 echo "System updated and upgraded"
-sleep 10
 
 # Download Java runtime environment 
 sudo apt-get install -y fontconfig openjdk-17-jre
@@ -15,7 +14,6 @@ sudo apt-get install -y fontconfig openjdk-17-jre
 #enter wait command finish the install
 wait
 echo "Java installed"
-sleep 10
 
 #This is the Debian package repository of Jenkins to automate installation and upgrade.
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \/usr/share/keyrings/jenkins-keyring.asc > /dev/null
@@ -32,27 +30,22 @@ wait
 #install Jenkins
 sudo apt install -y jenkins
 echo "JENKINS INSTALLED"
-sleep 5
 
 #install python3.10-venv
 sudo apt install -y python3.10-venv
 echo "PHTHON3.10-VENV INSTALLED"
-sleep 5
 
 #install python3-pip
 sudo apt install -y python3-pip
 echo "PYTHON3-PIP INSTALLED"
-sleep 5
 
 #install unzip
 sudo apt install -y unzip
 echo "UNZIP INSTALLED"
-sleep 5
 
 #install nginx
 sudo apt install -y nginx
 echo "NGINX INSTALLED"
-sleep 5
 
 #enter wait command finish the install
 wait
