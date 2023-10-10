ec6812b3ab341f9b393e5f90c5c26f544c89f85f
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-10 19:00:28 -0400

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -1,89 +1,44 @@
-# Configure aws provider
+########## Configure aws provider #########
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region = "us-east-1"
+  access_key = "" #enter your aws access_key
+  secret_key = "" #enter your aws secret_key
+  region     = "us-east-1" # Replace with your desired AWS region
   #profile = "Admin"
 }
 
-# Create VPC
-resource "aws_vpc" "vpc" {
-  cidr_block              = "172.19.0.0/16"
-  instance_tenancy        = "default"
-  enable_dns_hostnames    = true
-
-  tags      = {
-    Name    = "tf-made-vpc"
-  }
+########### Define the existing VPC by its ID ######### OPTIONAL ###
+data "aws_vpc" "" {
+  id = "" # Replace with your VPC ID
 }
 
-# Create a subnet within the VPC
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id #will cause error
-  cidr_block              = "172.19.0.0/16"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags      = {
-    Name    = "tf-made-subnet"
-  }
+################## Define the existing subnets by their IDs #########  OPTIONAL #######
+data "aws_subnet" "" {
+  id = "" # Replace with first subnet ID
 }
 
-# Create Security Groups
+data "aws_subnet" "" {
+  id = "" # Replace with your second subnet ID
+}
 
-resource "aws_security_group" "web_ssh" {
-  name        = "tf_made_sg"
-  description = "open ssh traffic"
-  vpc_id = aws_vpc.vpc.id
-
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+# Read the content of your Jenkins deploy script
+data "template_file" "" {
+  template = file("${path.module}/")
+}
+###################### CREATE INSTANCE #####################
+resource "aws_instance" "project4B" {
+  ami                         = "" # Replace with your desired AMI ID
+  instance_type               = "t2.medium"             # Change to your desired instance type
+  vpc_security_group_ids      = [""]
+  subnet_id                   = "s" # Use the ID of the second subnet
+  key_name                    = ""        # Replace with your SSH key name
+  user_data                   = data.template_file.jenkins_deploy_script.rendered
+  associate_public_ip_address = true # Ensure a public IP is assigned
   tags = {
-    "Name" : "tf_made_sg"
-    "Terraform" : "true"
+    "Name": ""
   }
-
 }
 
-# Create instance
-resource "aws_instance" "web_server01" {
-  ami = "ami-08c40ec9ead489470"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.my_subnet.id
-  security_groups = [aws_security_group.web_ssh.id]
-  #key_name = "ubuntuSandbox"
-
-  #user_data = "${file("deploy.sh")}"
-
-  tags = {
-    "Name" : "tf_made_instance"
-  }
-
-}
-
-
-output "instance_ip" {
-  value = aws_instance.web_server01.public_ip
+output "new_instance_ip" {
+  value       = aws_instance.project4B.public_ip
+  description = "The public IP of the newly created EC2 instance"
 }
