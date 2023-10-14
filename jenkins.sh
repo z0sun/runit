b7ad8b4c691a3062b7ec0b87ab7b5725587c8c0f
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-14 10:17:36 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y openjdk-11-jre
+sudo apt-get update -y
 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+# Install JDK
+sudo apt install openjdk-8-jdk -y
+sudo apt-get update -y
 
-sudo apt update
-sudo apt install jenkins -y
+# Download Jenkins keys
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update -y
+
+# Install & start Jenkins
+sudo apt-get install -y fontconfig openjdk-17-jre jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins >> ~/Status.txt
+
+# Get Jenkins unlock key
+echo "Jenkins Unlock Key:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# Update packages
+sudo apt update -y
