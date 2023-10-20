ae320f2b1402b72fffb6062d0dbc37ec0045fe3a
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-20 12:01:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -56,3 +56,107 @@ variable "sg_1_description" {}
 
 variable "aws_sg_2_name" {}
 variable "sg_2_description" {}
+
+#Set IAM role for instances
+variable "access_level"             {default = "ec2_full_access_role"}
+variable "ver"                      {default = "2012-10-17"}
+variable "action"                   {default = "sts:AssumeRole"}
+variable "effect"                   {default = "Allow"}
+variable "principal_service"        {default = "ec2.amazonaws.com"}
+variable "policy_arn"               {default = "arn:aws:iam::aws:policy/AdministratorAccess"}
+variable "instance_profile_name"    {default = "ec2_full_access_profile"}
+variable "machine_role"             {default = "dp5_machine_role"}
+
+#Set variables for instance tags
+variable "ec2_instance_tag_1" {
+  description = "tag for first instance"
+  type        = map(string)
+}
+
+variable "ec2_instance_tag_2" {
+  description = "tag for second instance"
+  type        = map(string)
+}
+
+variable "ec2_instance_tag_3" {
+  description = "tag for third instance"
+  type        = map(string)
+}
+
+#Set variable for user data scripts
+variable "ud_jenkins" {
+  description = "path to jenkins with python script"
+  default     = "user_data_5.1_jenkins.sh"
+}
+
+variable "ud_py_java" {
+  description = "path to python script"
+  default     = "user_data_5.1_java_python.sh"
+}
+
+#Set variables for ingress (incoming) traffic
+variable "ssh_access" {
+  description = "access through SSH on PORT 22"
+  type = object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  })
+  default = {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "http_access" {
+  description = "access through http on PORT 8000"
+  type = object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  })
+  default = {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "jenkins_access" {
+  description = "access jenkins on PORT 8080"
+  type = object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  })
+  default = {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Set variables for egress(outgoing) traffic
+variable "anywhere_outgoing" {
+  description = "go anywhere outbound with any protocol"
+  type = object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  })
+  default = {
+    from_port   = 0
+    to_port     = 65000
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
