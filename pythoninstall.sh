3eb13f176b0a32b5a0618ae5833738b5273fe15a
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-27 00:21:12 -0400

    Update pythoninstall.sh

--- pythoninstall.sh
+++ pythoninstall.sh
@@ -5,7 +5,7 @@ sudo apt update -y
 
 # Install Python
 sudo apt install -y software-properties-common
-sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo software-properties-common, add-apt-repository -y ppa:deadsnakes/ppa, python3.7, python3.7-venv, build-essential, libmysqlclient-dev, python3.7-dev
 sudo apt update -y
 sudo apt install -y python3.7 python3.7-venv
 
