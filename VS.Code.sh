21a1d733749122f69048a3af5984df177cc82cd0
Joseph White <135557197+z0sun@users.noreply.github.com>
2024-04-10 17:16:38 -0400

    Update VS.Code.sh

--- VS.Code.sh
+++ VS.Code.sh
@@ -3,11 +3,15 @@
 # Install code-server
 curl -fsSL https://code-server.dev/install.sh | sh
 
-# Enable code-server service
-sudo systemctl enable --now code-server@$USER
-
-# Wait for the service to start
-sleep 1
+# Check for systemctl command and enable code-server service
+if command -v systemctl &> /dev/null; then
+    sudo systemctl enable --now code-server@$USER
+    # Wait for the service to start
+    sleep 1
+    sudo systemctl restart code-server@$USER
+else
+    echo "systemctl is not available. Cannot enable or restart code-server service."
+fi
 
 # Configure code-server
 CONFIG_PATH="$HOME/.config/code-server/config.yaml"
@@ -15,6 +19,3 @@ echo "bind-addr: 0.0.0.0:8081
 auth: password
 password: abcd1234
 cert: false" > "$CONFIG_PATH"
-
-# Restart code-server service to apply configuration changes
-sudo systemctl restart code-server@$USER
